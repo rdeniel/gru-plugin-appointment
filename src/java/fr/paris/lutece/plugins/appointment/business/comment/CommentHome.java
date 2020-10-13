5222835fc696fd13fc62c3a6ef5183e50a8c4320
rdeniel <renaud.deniel@outlook.com>
2020-10-13 18:12:12 +0200

    RENDEZVOUS-459 add creation date and creator admin user fields for the comments

--- src/java/fr/paris/lutece/plugins/appointment/business/comment/CommentHome.java
+++ src/java/fr/paris/lutece/plugins/appointment/business/comment/CommentHome.java
@@ -35,7 +35,6 @@
  package fr.paris.lutece.plugins.appointment.business.comment;
 
 import fr.paris.lutece.plugins.appointment.service.AppointmentPlugin;
-import fr.paris.lutece.portal.business.user.AdminUser;
 import fr.paris.lutece.portal.service.plugin.Plugin;
 import fr.paris.lutece.portal.service.plugin.PluginService;
 import fr.paris.lutece.portal.service.spring.SpringContextService;
